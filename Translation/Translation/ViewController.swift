c3704a8733affb9b9e4b1cc7235005cd1ae300be
Nicolás Miari <nicolasmiari@gmail.com>
2017-03-04 11:34:56 +0900

    Added empty project

---
+++ Translation/Translation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Translation
+//
+//  Created by Nicolás Fernando Miari on 2017/03/04.
+//  Copyright © 2017 Try! Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
